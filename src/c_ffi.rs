b31fefdf8fbdf05e440bd7315a5ffd7ca9693d63
hanabi1224 <harlowmoo@gmail.com>
2018-07-28 09:51:59 -0700

    ffi (#5)

--- src/c_ffi.rs
+++ src/c_ffi.rs
@@ -1,28 +1,82 @@
+use super::IndexType;
 use super::AnnoyIndex;
+use super::AnnoyIndexSearchApi;
 
-/*
-use libc::{c_char, size_t};
+use libc::{c_char,size_t};
+use std::mem;
+use std::ffi::CStr;
+use std::slice;
 
 ffi_fn! {
-    fn load_annoy_index(path: *const u8, dimension: *const dim) -> *const AnnoyIndex {
-        let len = unsafe { CStr::from_ptr(pattern).to_bytes().len() };
-        let pat = pattern as *const u8;
-        let mut err = Error::new(ErrorKind::None);
-        let re = rure_compile(
-            pat, len, RURE_DEFAULT_FLAGS, ptr::null(), &mut err);
-        if err.is_err() {
-            let _ = writeln!(&mut io::stderr(), "{}", err);
-            let _ = writeln!(
-                &mut io::stderr(), "aborting from rure_compile_must");
-            unsafe { abort() }
-        }
-        re
+    fn load_annoy_index(path: *const c_char, dimension: i32, index_type: u8) -> *const AnnoyIndex {
+        //let ru_dimension = dimension;
+        let c_str_path =  unsafe { CStr::from_ptr(path) };
+        let ru_path = c_str_path.to_str().unwrap();
+        let ru_index_type:IndexType = unsafe { mem::transmute(index_type) };
+        let index = AnnoyIndex::load(dimension, ru_path, ru_index_type);
+        return Box::into_raw(Box::new(index));
     }
 }
-*/
 
 ffi_fn! {
     fn free_annoy_index(index: *const AnnoyIndex){
         unsafe { Box::from_raw(index as *mut AnnoyIndex); }
+    }
+}
+
+ffi_fn! {
+    fn get_dimension(index_ptr: *const AnnoyIndex) -> i32{
+        let index = unsafe { &*index_ptr };
+        return index.dimension;
+    }
+}
+
+ffi_fn! {
+    fn get_item_vector(index_ptr: *const AnnoyIndex, item_index: i64) -> *const f32{
+        let index = unsafe{&*index_ptr};
+        let item_vec = index.get_item_vector(item_index);
+        return item_vec.as_ptr();
+    }
+}
+
+#[repr(C)]
+pub struct AnnoyIndexSearchResult_FFI {
+    pub count: size_t,
+    pub id_list: *const i64,
+    pub distance_list: *const f32,
+}
+
+ffi_fn! {
+    fn get_nearest(
+        index_ptr: *const AnnoyIndex,
+        query_vector_ptr: *const f32,
+        n_results: size_t,
+        search_k: i32,
+        should_include_distance: bool) -> *const AnnoyIndexSearchResult_FFI
+    {
+        let index = unsafe{&*index_ptr};
+        let query_vector = unsafe { slice::from_raw_parts(query_vector_ptr, index.dimension as usize) };
+        let results = index.get_nearest(query_vector, n_results, search_k, should_include_distance);
+        let results_len = results.len();
+        let mut id_list:Vec<i64> = Vec::with_capacity(results_len);
+        let mut distance_list:Vec<f32> = Vec::with_capacity(results_len);
+        for result in results{
+            id_list.push(result.id);
+            distance_list.push(result.distance);
+        }
+
+        let result_ffi = AnnoyIndexSearchResult_FFI{
+            count: results_len,
+            id_list: id_list.as_ptr(),
+            distance_list: distance_list.as_ptr(),
+        };
+
+        return Box::into_raw(Box::new(result_ffi));;
+    }
+}
+
+ffi_fn! {
+    fn free_search_result(search_result_ptr: *const AnnoyIndexSearchResult_FFI){
+        unsafe { Box::from_raw(search_result_ptr as *mut AnnoyIndexSearchResult_FFI); }
     }
 }
