d6f962a65678dc7f5487b88787d17ebd2a43e44a
hanabi1224 <harlowmoo@gmail.com>
2018-07-28 22:08:14 -0700

    dotnet ffi

---
+++ dotnet/RuAnnoy/AnnoyIndex.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace RuAnnoy
+{
+    public class AnnoyIndex : IAnnoyIndex, IDisposable
+    {
+        private readonly IntPtr _indexPtr;
+
+        public AnnoyIndex(
+            string path,
+            int dimension,
+            IndexType type)
+        {
+            Dimension = dimension;
+            _indexPtr = NativeMethods.LoadAnnoyIndex(path, dimension, type);
+        }
+
+        public int Dimension { get; }
+
+        public static IAnnoyIndex Load(
+            string path,
+            int dimension,
+            IndexType type)
+        {
+            return new AnnoyIndex(path, dimension, type);
+        }
+
+        public ReadOnlyMemory<float> GetItemVector(long itemIndex)
+        {
+            var itemVectorPtr = NativeMethods.GetItemVector(_indexPtr, itemIndex);
+            var itemVector = new float[Dimension];
+            Marshal.Copy(itemVectorPtr, itemVector, 0, Dimension);
+            return itemVector;
+        }
+
+        public AnnoyIndexSearchResult GetNearest(
+            ReadOnlyMemory<float> queryVector,
+            ulong nResult,
+            int searchK,
+            bool shouldIncludeDistance)
+        {
+            var searchResultPtr = NativeMethods.GetNearest(
+                  _indexPtr,
+                  queryVector.ToArray(),
+                  new UIntPtr(nResult),
+                  searchK,
+                  shouldIncludeDistance);
+            try
+            {
+                return AnnoyIndexSearchResult.LoadFromPtr(searchResultPtr);
+            }
+            finally
+            {
+                NativeMethods.FreeSearchResult(searchResultPtr);
+            }
+        }
+
+        public void Dispose()
+        {
+            NativeMethods.FreeAnnoyIndex(_indexPtr);
+        }
+    }
+}
---
+++ dotnet/RuAnnoy/NativeMethods.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace RuAnnoy
+{
+    internal class NativeMethods
+    {
+        const string DLLPATH = @"ru_annoy";
+
+        [DllImport(DLLPATH, EntryPoint = "load_annoy_index", CharSet = CharSet.Ansi)]
+        internal static extern IntPtr LoadAnnoyIndex(
+            string path,
+            Int32 dimension,
+            IndexType indexType);
+
+        [DllImport(DLLPATH, EntryPoint = "free_annoy_index", CharSet = CharSet.Ansi)]
+        internal static extern IntPtr FreeAnnoyIndex(IntPtr index);
+
+        [DllImport(DLLPATH, EntryPoint = "get_dimension", CharSet = CharSet.Ansi)]
+        internal static extern Int32 GetDimension(IntPtr index);
+
+        [DllImport(DLLPATH, EntryPoint = "get_item_vector", CharSet = CharSet.Ansi)]
+        internal static extern IntPtr GetItemVector(IntPtr index, Int64 itemIndex);
+
+        [DllImport(DLLPATH, EntryPoint = "get_nearest", CharSet = CharSet.Ansi)]
+        internal static extern IntPtr GetNearest(
+            IntPtr index,
+            float[] vector,
+            UIntPtr nResults,
+            Int32 searchK,
+            bool shouldIncludeDistance);
+
+        [DllImport(DLLPATH, EntryPoint = "free_search_result", CharSet = CharSet.Ansi)]
+        internal static extern IntPtr FreeSearchResult(IntPtr searchResult);
+
+        [DllImport(DLLPATH, EntryPoint = "get_result_count", CharSet = CharSet.Ansi)]
+        internal static extern UIntPtr GetResultCount(IntPtr searchResult);
+
+        [DllImport(DLLPATH, EntryPoint = "get_id_list", CharSet = CharSet.Ansi)]
+        internal static extern IntPtr GetIdList(IntPtr searchResult);
+
+        [DllImport(DLLPATH, EntryPoint = "get_distance_list", CharSet = CharSet.Ansi)]
+        internal static extern IntPtr GetDistanceList(IntPtr searchResult);
+    }
+}
