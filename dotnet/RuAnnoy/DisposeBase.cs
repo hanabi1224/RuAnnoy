5e032596b755bfb97ad0d57fc2f94ef1c9fdc211
hanabi1224 <harlowmoo@gmail.com>
2018-12-10 08:48:49 +0000

    memmap0.7 (#20)

---
+++ dotnet/RuAnnoy/DisposeBase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace RuAnnoy
+{
+    public abstract class DisposeBase : IDisposable
+    {
+        private bool isDisposed;
+
+        ~DisposeBase()
+        {
+            this.Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (this.isDisposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                this.DisposeResources();
+            }
+
+            this.isDisposed = true;
+        }
+
+        protected abstract void DisposeResources();
+    }
+}
