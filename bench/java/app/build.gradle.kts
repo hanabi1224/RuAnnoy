ef1483a021b02b1fb1336ba62e6ee7776923b542
hanabi1224 <harlowmoo@gmail.com>
2021-06-10 10:44:14 +0800

    fix

--- bench/java/app/build.gradle.kts
+++ bench/java/app/build.gradle.kts
@@ -23,20 +23,7 @@ dependencies {
     implementation("com.github.hanabi1224:RuAnnoy:master-SNAPSHOT")
     implementation("com.spotify:annoy:0.2.6")
     
-    // Align versions of all Kotlin components
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-
-    // Use the Kotlin JDK 8 standard library.
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-
-    // This dependency is used by the application.
-    implementation("com.google.guava:guava:29.0-jre")
-
-    // Use the Kotlin test library.
     testImplementation("org.jetbrains.kotlin:kotlin-test")
-
-    // Use the Kotlin JUnit integration.
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 }
 
 application {
