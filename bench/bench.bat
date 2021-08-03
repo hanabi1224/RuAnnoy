93aba67a2f7bb73c672cb7e9a1f772d9faf4fbb6
hanabi1224 <harlowmoo@gmail.com>
2021-08-03 22:24:09 +0800

    updt

--- bench/bench.bat
+++ bench/bench.bat
@@ -1,20 +1,20 @@
 python3 -m pip install -U wheel annoy
 
-python3 builder.py 128 10000
+python3 builder.py 256 10000
 
-python3 bencher.py 128 10000 200 1000
+python3 bencher.py 256 10000 200 1000
 
 pushd rust
-cargo run -q --release -- 128 10000 200 1000
-cargo +nightly run -q --release --all-features -- 128 10000 200 1000
+cargo run -q --release -- 256 10000 200 1000
+cargo +nightly run -q --release --all-features -- 256 10000 200 1000
 popd
 
 pushd dart
-dart pub get && dart run main.dart 128 10000 200 1000
+dart pub get && dart run main.dart 256 10000 200 1000
 popd
 
-dotnet run -p dotnet -c Release -- --dim 128 --size 10000 --n-result 200 --n-loop 1000
+dotnet run -p dotnet -c Release -- --dim 256 --size 10000 --n-result 200 --n-loop 1000
 
 pushd java
-gradlew run --args="128 10000 200 1000"
+gradlew run --args="256 10000 200 1000"
 popd
