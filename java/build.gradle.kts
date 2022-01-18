265bf0963e1cc5901ff59de3bd7a9ee631343584
hanabi1224 <harlowmoo@gmail.com>
2022-01-18 16:12:31 +0800

    codecov on GH action

--- java/build.gradle.kts
+++ java/build.gradle.kts
@@ -10,7 +10,7 @@ import org.gradle.api.tasks.testing.logging.*
 
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
-    kotlin("jvm") version "1.6.0"
+    kotlin("jvm") version "1.6.10"
 
     // Apply the java-library plugin for API and implementation separation.
     `java-library`
